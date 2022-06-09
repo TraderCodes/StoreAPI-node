fa9a9a43ce870cc6646835e47cab264ee224d2e4
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 06:43:38 -0700

    Setup : router

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const res = require('express/lib/response');
-const  connectDB = require('./db/connect')
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
   res.send('<h1>storeAPI</h1><a href="/api/v1/products">products routes</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(3000, console.log('server live'));
   } catch (error) {
     console.log('error');
