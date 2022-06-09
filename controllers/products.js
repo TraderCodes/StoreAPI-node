d4ea57becfa87cb0a79119efa7b9087b6de3b049
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 09:27:19 -0700

    Refactir queryObeject

--- controllers/products.js
+++ controllers/products.js
@@ -5,9 +5,14 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products: products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
-
-  res.status(200).json({ products: products, nbHits: products.length });
+  const { featured } = req.query;
+  const queryObject = {};
+  // first we check if there is featured in the link which is the query
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
