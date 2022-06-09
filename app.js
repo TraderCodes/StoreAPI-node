ccf69c4f1ecf2485db2687b61b923b402d8eebde
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 02:09:42 -0700

    Setup express app server

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+// import middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const res = require('express/lib/response');
+
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>storeAPI</h1><a href="/api/v1/products">products routes</a>');
+});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+const start = async () => {
+  try {
+    app.listen(3000, console.log('server live'));
+  } catch (error) {
+    console.log('error');
+  }
+};
+start();
