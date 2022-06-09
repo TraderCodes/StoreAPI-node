768dbde4f4e9cd2659553c7a4a1f6df47a61822d
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 07:19:05 -0700

    Setup Schema

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name needed'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'product price needed'],
+  },
+
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'macros'],
+      message: 'value not supported',
+    },
+    // enum:['ikea','liddy','caressa','macros']
+  },
+});
+
+// export schema
+module.exports = mongoose.model('product', productSchema);
